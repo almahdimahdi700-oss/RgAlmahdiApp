59b5906a9863a21bab56ed1893f9bff576717fd0
almahdimahdi700-oss <almahdimahdi700@gmail.com>
2026-05-10 09:25:22 +0300

    Update MainActivity.java

--- app/src/main/java/com/rgalmahdi/app/MainActivity.java
+++ app/src/main/java/com/rgalmahdi/app/MainActivity.java
@@ -1,7 +1,6 @@
-package com.rgalmahdi.app;
+package com.rgalmahdi.blogspot.app;
 
 import android.os.Bundle;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,34 +19,16 @@ public class MainActivity extends AppCompatActivity {
         swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
         webView = findViewById(R.id.webView);
 
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setDomStorageEnabled(true);
-        webSettings.setLoadWithOverviewMode(true);
-        webSettings.setUseWideViewPort(true);
-        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        
+        webView.loadUrl("https://rgalmahdi.blogspot.com");
 
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                swipeRefreshLayout.setRefreshing(false);
-            }
-
-            @Override
-            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-                // هذه الصفحة تطلع لو موقعك طافي
-                String errorHtml = "<html><body style='background-color:#000; color:#FFD700; text-align:center; padding-top:100px; font-family:sans-serif;'>" +
-                        "<h1>رق المهدي ن1 ❍〠〄</h1>" +
-                        "<h3>الموقع تحت الصيانة حالياً</h3>" +
-                        "<p>اسحب للأسفل للتحديث</p>" +
-                        "<p>أو تأكد من اتصالك بالانترنت</p>" +
-                        "</body></html>";
-                view.loadDataWithBaseURL(null, errorHtml, "text/html", "UTF-8", null);
-            }
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            webView.reload();
+            swipeRefreshLayout.setRefreshing(false);
         });
-
-        webView.loadUrl("https://rgalmahdi.com");
-        swipeRefreshLayout.setOnRefreshListener(() -> webView.reload());
     }
 
     @Override
